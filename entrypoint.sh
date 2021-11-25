7274d19069e51a2be9fe49596eb96e79216453ea
nkosinathi <nkosinathisola@gmail.com>
2021-11-25 21:59:10 +0200

    Add gunicorn production code

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 # entrypoint is set in docker-compose file which describes multicontainer environment while referencing the Dockerfile.
 
 # use gunicorn in production
-gunicorn --bind 0.0.0.0:${APP_PORT} run:app
+# gunicorn --bind 0.0.0.0:${APP_PORT} run:app
+gunicorn app:app -w 2 --threads 2 -b 0.0.0.0:8000
 
 # use flask cli for development
 # flask run --host 0.0.0.0 --port ${APP_PORT}
